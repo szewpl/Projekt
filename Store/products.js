3fb9b9b6b98c0c660dea2bf1c5fe41e35beb6d67
szewpl <84736858+szewpl@users.noreply.github.com>
2021-05-29 00:23:19 +0200

    prawie koniec creatora pog

--- Store/products.js
+++ Store/products.js
@@ -2,21 +2,17 @@ var addprod = document.getElementById('addproduct');
 
 addprod.setAttribute('style','border-top:2px solid #ff3333;width:75%;margin-left:auto;margin-right:auto;border-bottom:2px solid #ff3333;');
 
-var select = document.createElement("select");
-var labelsel = document.createElement("label");
-var zdj = document.createElement("input");
-var labelprice = document.createElement("label");
-var price = document.createElement("input");
-var btadd = document.getElementById("addprod");
-var shortdes = document.createElement("input");
-var description = [];
 
-price.type = "number";
-shortdes.type = "text";
-labelprice.innerText = "wpisz cene";
-zdj.type = "url";
-zdj.placeholder = "Wpisz adres do obrazu";
-labelsel.innerHTML = "Kategoria";
+var k = document.getElementsByClassName("keys");
+var v = document.getElementsByClassName("values");
+var addprods = document.getElementById("addproducts");
+var select = document.getElementById("addselect");
+var zdj = document.getElementById("url");
+var price = document.getElementById("price");
+var textarea = document.getElementById("textarea");
+var btadd = document.getElementById("addprod");
+const description = {};
+var counter = 0;
 
 for(let i=0;i<Categories.length;i++)
 {
@@ -28,10 +24,28 @@ for(let i=0;i<Categories.length;i++)
     }
 }
 
+function addinputs(){
+    var keys = document.createElement('input');
+    var values = document.createElement('input');
+    var br = document.createElement('br');
+
+    keys.classList.add("keys");
+    values.classList.add("values");
+    keys.placeholder = "Nazwa dany";
+    values.placeholder = "Wartosc danej";
+
+    addprods.appendChild(keys);
+    addprods.appendChild(values);
+    addprods.appendChild(br);
+    counter++;
+}
 
 class createProduct{
     dodajopis(){
-        description.push({"elo":"elo"});
+        for(let i = 0;i<k.length;i++)
+        {
+           description[k[i].value] = v[i].value;
+        }
     }
     savels(){
         /*if(localStorage.getItem("product_id") == null)
@@ -40,16 +54,20 @@ class createProduct{
         }else{
             id = localStorage.setItem("product_id",parseInt(localStorage.getItem("product_id"))+1);
         }*/
-        var prod = [{"Id":parseInt(localStorage.getItem("product_id")),"Id kategori":select.value,"Img":zdj.value,"Cena":price.value,"Description":description,"Krótki opis":2}];
+        const prod = {"Id":parseInt(localStorage.getItem("product_id")),"Id kategori":select.value,"Img":zdj.value,"Cena":price.value,"Krótki opis":textarea.value,"Dane techniczne":description};
         console.log(prod);
+        
         
         //localStorage.setItem("Produkt"+parseInt(localStorage.getItem("product_id")),);
     }
     
 }
-
 var c = new createProduct();
-btadd.addEventListener("click",()=>{c.savels();c.dodajopis()});
+btadd.addEventListener("click",()=>{
+    c.dodajopis();
+    c.savels();
+});
+
 
 /*if(localStorage.getItem("product_id") == null){
     localStorage.setItem("product_id",0);
