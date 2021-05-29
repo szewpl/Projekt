b52bf93799df58993955b5ae101241f593582dcd
szewpl <84736858+szewpl@users.noreply.github.com>
2021-05-30 00:13:04 +0200

    essa

--- Store/products.js
+++ Store/products.js
@@ -20,20 +20,11 @@ localStorage.setItem("Produkt"+cwiczeniazkodowania.Id,JSON.stringify(cwiczeniazk
 
 class createProduct{
     savels(){
-        if(localStorage.getItem("product_id") < 6 || localStorage.getItem("product_id") == null)
-        {
-            localStorage.setItem("product_id",6);
-            var prod={"Id":parseInt(localStorage.getItem("product_id")),"category_id":select.value,"url":zdj.value,"Cena":price.value,"Opis":textarea.value,"Nazwa":nameprod.value};
-        
-            localStorage.setItem("Produkt"+parseInt(localStorage.getItem("product_id")),JSON.stringify(prod));
-            location.reload();
-        }else{
-            localStorage.setItem("product_id",parseInt(localStorage.getItem("product_id"))+1);
-            var prod={"Id":parseInt(localStorage.getItem("product_id")),"category_id":select.value,"url":zdj.value,"Cena":price.value,"Opis":textarea.value,"Nazwa":nameprod.value};
-        
-            localStorage.setItem("Produkt"+parseInt(localStorage.getItem("product_id")),JSON.stringify(prod));
-            location.reload();
-        }
+        localStorage.setItem("product_id",parseInt(localStorage.getItem("product_id"))+1);
+        var prod={"Id":parseInt(localStorage.getItem("product_id")),"category_id":select.value,"url":zdj.value,"Cena":price.value,"Opis":textarea.value,"Nazwa":nameprod.value};
+    
+        localStorage.setItem("Produkt"+parseInt(localStorage.getItem("product_id")),JSON.stringify(prod));
+        location.reload();
     }
 }
 
