f0440c13586e57abfdf69d1e3d11fe9d99ccdbc5
szewpl <84736858+szewpl@users.noreply.github.com>
2021-06-10 20:07:48 +0200

    to co krzysiek odjebał

--- Store/config.js
+++ Store/config.js
@@ -1,44 +1,47 @@
 const Menu = {
-    logo:{
+    logo: {
         id: 1,
         name: "Sklep z zabawkami 🧸",
     },
-    specialoffers:{
-        id:2,
+    specialoffers: {
+        id: 2,
         name: "Oferty specjalne",
     },
-    useraccount:{
-        id:3,
+    useraccount: {
+        id: 3,
         name: "Konto użytkownika",
+    },
+    darkmode: {
+        id: 4,
+        name: ["🌙", "☀️"],
     },
 
 };
 
 const select = {
-    0:{
-        id:0,
-        name:"Cena od najmniejszej"
+    0: {
+        id: 0,
+        name: "Cena od najmniejszej"
     },
-    1:{
-        id:1,
-        name:"Cena od najwiekszej"
+    1: {
+        id: 1,
+        name: "Cena od najwiekszej"
     },
 }
 
 
 
-for(let i =0;i<localStorage.getItem("product_id");i++){
-    if(localStorage.getItem("koszykProdukt"+i) == null)
-    {
-        var obj = JSON.parse(localStorage.getItem("Produkt"+i));
+for (let i = 0; i < localStorage.getItem("product_id"); i++) {
+    if (localStorage.getItem("koszykProdukt" + i) == null) {
+        var obj = JSON.parse(localStorage.getItem("Produkt" + i));
         obj["ilosc"] = 0;
         obj["Cena"] = 0;
-        localStorage.setItem("koszykProdukt"+i,JSON.stringify(obj));
+        localStorage.setItem("koszykProdukt" + i, JSON.stringify(obj));
     }
 }
 
-if(localStorage.getItem("konto") == null){
-    localStorage.setItem("konto",0);
+if (localStorage.getItem("konto") == null) {
+    localStorage.setItem("konto", 0);
 }
 
 
@@ -47,111 +50,130 @@ let menu = document.getElementById("menu");
 let logo = document.getElementById("logo");
 let specialoffers = document.getElementById("specialoffers");
 let useraccount = document.getElementById("useraccount");
-specialoffers.addEventListener("click",()=>{window.location.href = "ofertyspecjalne.html"});
+let dmode = document.getElementById("darkmode");
+specialoffers.addEventListener("click", () => { window.location.href = "ofertyspecjalne.html" });
 
 
 logo.innerHTML = Menu.logo.name;
-logo.setAttribute('style','width:33.33%;text-align:center;border-right:5px solid white;');
-logo.addEventListener('click',()=>{window.location.href = "index.html"});
+logo.setAttribute('style', 'width:32%;text-align:center;border-right:5px solid white;');
+logo.addEventListener('click', () => { window.location.href = "index.html" });
 specialoffers.innerHTML = Menu.specialoffers.name;
-specialoffers.setAttribute('style','width:33.33%;text-align:center;border-right:5px solid white;');
+specialoffers.setAttribute('style', 'width:32%;text-align:center;border-right:5px solid white;');
 useraccount.innerHTML = Menu.useraccount.name;
-useraccount.setAttribute('style','width:33.33%;text-align:center;');
+useraccount.setAttribute('style', 'width:32%;text-align:center;');
+dmode.innerHTML = Menu.darkmode.name[0];
+dmode.setAttribute('style', 'width: 4%; text-align:center; border-left:5px solid white;');
 
-menu.setAttribute('style','background-color: #ff3333;display:flex;height:65px;color:white;font-size:32px;line-height: 65px;cursor:pointer;margin-bottom:10px;');
+menu.setAttribute('style', 'background-color: #ff3333;display:flex;height:65px;color:white;font-size:32px;line-height: 65px;cursor:pointer;margin-bottom:10px;');
 
 
 var wysz = document.getElementById("wysz");
 var sel = document.createElement("select");
-if(wysz){
+if (wysz) {
     sel.id = "sel";
-    for(let i=0;i<2;i++)
-    {
-            var option = document.createElement("option");
-            option.text = select[i].name;
-            option.value = select[i].id;
-            
-            sel.add(option);
+    for (let i = 0; i < 2; i++) {
+        var option = document.createElement("option");
+        option.text = select[i].name;
+        option.value = select[i].id;
+
+        sel.add(option);
     }
-    sel.addEventListener("click",()=>{wybor();});
+    sel.addEventListener("click", () => { wybor(); });
     wysz.appendChild(sel);
 }
 
-
+sel.setAttribute('style', 'margin: 0 10px 5px 10px; border: 1px solid #ff3333;');
 
 var searchinput = document.getElementById("wyszukiwarka");
-if(searchinput){
-    searchinput.addEventListener("input",()=>{search();});
+if (searchinput) {
+    searchinput.addEventListener("input", () => { search(); });
 }
 
-if(localStorage.getItem("ilosczkoszyka")==null){
-    localStorage.setItem("ilosczkoszyka",0);
+if (localStorage.getItem("ilosczkoszyka") == null) {
+    localStorage.setItem("ilosczkoszyka", 0);
 }
 
 var divbasket = document.createElement("div");
 
-function addingtobasket(obj){
-    localStorage.setItem("ilosczkoszyka",(parseInt(localStorage.getItem("ilosczkoszyka")))+1);
-    obj["ilosc"] = obj["ilosc"] +1;
-    obj["Cena"] = obj["Cena"] + JSON.parse(localStorage.getItem("Produkt"+obj.Id)).Cena;
-    localStorage.setItem("koszykProdukt"+obj.Id,JSON.stringify(obj)); 
-    
+divbasket.setAttribute('style', 'margin: 0 10px 5px 10px; border:1px solid #ff3333; padding: 0 5px')
+
+function addingtobasket(obj) {
+    localStorage.setItem("ilosczkoszyka", (parseInt(localStorage.getItem("ilosczkoszyka"))) + 1);
+    obj["ilosc"] = obj["ilosc"] + 1;
+    obj["Cena"] = obj["Cena"] + JSON.parse(localStorage.getItem("Produkt" + obj.Id)).Cena;
+    localStorage.setItem("koszykProdukt" + obj.Id, JSON.stringify(obj));
+
     divbasket.innerHTML = "Koszyk: " + localStorage.getItem("ilosczkoszyka");
-    if(wysz){
-    wysz.appendChild(divbasket);
+    if (wysz) {
+        wysz.appendChild(divbasket);
     }
 }
 
-function deletepr(obj){
-    localStorage.setItem("ilosczkoszyka",(parseInt(localStorage.getItem("ilosczkoszyka")))-1);
-    obj["ilosc"] = obj["ilosc"] -1;
-    obj["Cena"] = obj["Cena"] - JSON.parse(localStorage.getItem("Produkt"+obj.Id)).Cena;
-    localStorage.setItem("koszykProdukt"+obj.Id,JSON.stringify(obj)); 
-    
+function deletepr(obj) {
+    localStorage.setItem("ilosczkoszyka", (parseInt(localStorage.getItem("ilosczkoszyka"))) - 1);
+    obj["ilosc"] = obj["ilosc"] - 1;
+    obj["Cena"] = obj["Cena"] - JSON.parse(localStorage.getItem("Produkt" + obj.Id)).Cena;
+    localStorage.setItem("koszykProdukt" + obj.Id, JSON.stringify(obj));
+
     divbasket.innerHTML = "Koszyk: " + localStorage.getItem("ilosczkoszyka");
-    if(wysz){
-    wysz.appendChild(divbasket);
+    if (wysz) {
+        wysz.appendChild(divbasket);
     }
 }
 
-function deleteprallf(obj){
-    localStorage.setItem("ilosczkoszyka",(parseInt(localStorage.getItem("ilosczkoszyka")))-obj.ilosc);
-    obj["ilosc"] = obj["ilosc"] -obj.ilosc;
+function deleteprallf(obj) {
+    localStorage.setItem("ilosczkoszyka", (parseInt(localStorage.getItem("ilosczkoszyka"))) - obj.ilosc);
+    obj["ilosc"] = obj["ilosc"] - obj.ilosc;
     obj["Cena"] = 0;
-    localStorage.setItem("koszykProdukt"+obj.Id,JSON.stringify(obj)); 
-    
+    localStorage.setItem("koszykProdukt" + obj.Id, JSON.stringify(obj));
+
     divbasket.innerHTML = "Koszyk: " + localStorage.getItem("ilosczkoszyka");
-    if(wysz){
-    wysz.appendChild(divbasket);
+    if (wysz) {
+        wysz.appendChild(divbasket);
     }
 }
 
-divbasket.addEventListener("click",()=>{window.location.href = "displaybasket.html";});
+divbasket.addEventListener("click", () => { window.location.href = "displaybasket.html"; });
 divbasket.innerHTML = "Koszyk: " + localStorage.getItem("ilosczkoszyka");
 
-if(wysz){
+if (wysz) {
     wysz.appendChild(divbasket);
 }
 
-if(sessionStorage.getItem("zalogowanyjako") != null){
-    useraccount.innerHTML = '\xa0 Witaj, '+sessionStorage.getItem("zalogowanyjako");
-    useraccount.addEventListener("click",()=>{
-    if(confirm("Czy chcesz sie wylogowac?")){
-        sessionStorage.removeItem("zalogowanyjako");
-        window.location.reload();
-    }
-});
-}else{
-    document.getElementById('useraccount').addEventListener('click',()=>{window.location.href = "form_account.html";});
+if (sessionStorage.getItem("zalogowanyjako") != null) {
+    useraccount.innerHTML = '\xa0 Witaj, ' + sessionStorage.getItem("zalogowanyjako");
+    useraccount.addEventListener("click", () => {
+        if (confirm("Czy chcesz sie wylogowac?")) {
+            sessionStorage.removeItem("zalogowanyjako");
+            window.location.reload();
+        }
+    });
+} else {
+    document.getElementById('useraccount').addEventListener('click', () => { window.location.href = "form_account.html"; });
 }
 
-if(sessionStorage.getItem("zalogowanyjako") == "admin"){
+if (sessionStorage.getItem("zalogowanyjako") == "admin") {
     var bt = document.createElement("button");
 
-    bt.innerHTML= "Stworz produkt";
-    bt.addEventListener("click",()=>{window.location.href = "creator.html"});
+    bt.innerHTML = "Stworz produkt";
+    bt.addEventListener("click", () => { window.location.href = "creator.html" });
 
-    if(wysz){
+    if (wysz) {
         wysz.appendChild(bt);
     }
+}
+
+
+dmode.addEventListener('click', (e) => {
+    if (dmode.textContent == Menu.darkmode.name[0])
+        dmode.innerHTML = Menu.darkmode.name[1];
+    else
+        dmode.innerHTML = Menu.darkmode.name[0];
+    const darkMode = document.body.classList.toggle('dark-mode');
+    e.target.blur();
+    localStorage.setItem('dark-mode', darkMode);
+});
+
+if (JSON.parse(localStorage.getItem('dark-mode'))) {
+    document.body.classList.add('dark-mode');
 }
